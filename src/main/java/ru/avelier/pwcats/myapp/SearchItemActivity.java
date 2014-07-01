0b752fb7dfb393be3e7c916a9b48c2200b16a322
Adelier <adelier@hotmail.com>
2014-07-01 15:15:48 +0400

    add icons!

--- src/main/java/ru/avelier/pwcats/myapp/SearchItemActivity.java
+++ src/main/java/ru/avelier/pwcats/myapp/SearchItemActivity.java
@@ -7,6 +7,9 @@ import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -17,6 +20,9 @@ import android.widget.*;
 import ru.avelier.pwcats.db.DbItemsContract.ItemsEntry;
 import ru.avelier.pwcats.db.DbItemsHelper;
 
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 public class SearchItemActivity extends Activity {
@@ -48,7 +54,6 @@ public class SearchItemActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_item);
         // fill server spinner
-        // TODO remember selection
         Spinner spinner = (Spinner) findViewById(R.id.spinner_server);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.servers, android.R.layout.simple_spinner_item);
@@ -87,11 +92,17 @@ public class SearchItemActivity extends Activity {
     // TODO optimise
     // TODO case-insensitive. Maybe create 1 more row with all in lower case. Ибо sqlite буржуйский.
     public void search(String subname) {
+        // stop loading icons
+        for (AsyncTask<String, Void, Bitmap> task : loadIconTasks) {
+            task.cancel(true);
+        }
+        loadIconTasks.clear();
+        // process search
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor c = null;
         try {
             // || things because of https://code.google.com/p/android/issues/detail?id=3153
-            String SELECT_WHERE = "SELECT _id, name FROM items WHERE name LIKE '%' || ? || '%' OR name LIKE '%' || ? || '%' LIMIT 0, 100";
+            String SELECT_WHERE = "SELECT _id, name FROM items WHERE name LIKE '%' || ? || '%' OR name LIKE '%' || ? || '%' LIMIT 0, 50";
             String[] binds = new String[]{subname, ("" + subname.charAt(0)).toUpperCase() + subname.substring(1)};
             c = db.rawQuery(SELECT_WHERE, binds);
         } catch (Exception e) {
@@ -105,25 +116,7 @@ public class SearchItemActivity extends Activity {
         insertPoint.removeAllViewsInLayout();
         if (c.moveToFirst()) {
             do {
-                LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                View v = vi.inflate(R.layout.search_item_line, null);
-
-                // TODO icon
-                TextView itemId = (TextView) v.findViewById(R.id.textCatName);
-                final int id = c.getInt(0);
-                itemId.setText(id + "");
-                final String itemName = c.getString(1);
-                TextView itemNameView = (TextView) v.findViewById(R.id.textCatTitle);
-                itemNameView.setText(itemName);
-                v.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        viewItemDetails(id, itemName);
-
-                    }
-                });
-// insert into main view
-                insertPoint.addView(v, insertPoint.getChildCount(), new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                add_item_line(c, insertPoint);
             } while (c.moveToNext());
         } else { // empty
             Log.d("db", "empty select for subname=" + subname);
@@ -132,6 +125,38 @@ public class SearchItemActivity extends Activity {
 
 
         c.close();
+    }
+
+    private final List<AsyncTask<String, Void, Bitmap>> loadIconTasks = new LinkedList<AsyncTask<String, Void, Bitmap>>();
+
+    private void add_item_line(Cursor c, ViewGroup insertPoint) {
+        LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = vi.inflate(R.layout.search_item_line, null);
+
+        final int id = c.getInt(0);
+        final String itemName = c.getString(1);
+
+// load icon
+        AsyncTask<String, Void, Bitmap> loadIconTask = new DownloadImageTask((ImageView) v.findViewById(R.id.itemIcon));
+        loadIconTasks.add(loadIconTask);
+        loadIconTask.execute(String.format("http://www.pwdatabase.com/images/icons/generalm/%d.gif", id));
+// fill id (hidden)
+        TextView itemId = (TextView) v.findViewById(R.id.textCatName);
+        itemId.setText(id + "");
+        itemId.setVisibility(View.GONE);
+// fill item name
+        TextView itemNameView = (TextView) v.findViewById(R.id.textCatTitle);
+        itemNameView.setText(itemName);
+//
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewItemDetails(id, itemName);
+            }
+        });
+
+// insert into main view
+        insertPoint.addView(v, insertPoint.getChildCount(), new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
     }
 
 
@@ -149,4 +174,33 @@ public class SearchItemActivity extends Activity {
         startActivity(intent);
     }
 
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urlDisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urlDisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+            // TODO think of bmImage.setImageURI()
+            float scale = (float)bmImage.getHeight() / result.getHeight();
+            bmImage.setScaleX( scale );
+            bmImage.setScaleY( scale );
+        }
+    }
 }
